1267f4107664493feba747e22af11a978e39dda4
Ben Taylor <taylorbf@gmail.com>
2016-07-22 22:26:38 -0400

    fixed

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 function setup() {
-  createCanvas(windowWidth,windowHeight)
+  createCanvas(1000,1000)
 }
 
 function draw() {
-  background(255)
+  background(200,200,200)
   fill(0,100,250,100)
   stroke(255,100)
 	for (var i=0;i<touches.length;i++) {
-		ellipse(touches[i].x, touches[i].y, 80, 80)
+		ellipse(touches[i].x, touches[i].y, 300, 300)
 	}
+}
+
+function touchStarted() {
+  return false
 }
